bf76d47c7c1b659f0db21fd01a3136fd2ce9d796
Diego Munoz <dmunozs86@gmail.com>
2011-05-13 12:33:02 -0400

    merge de tests.py

--- bolsa_trabajo/tests.py
+++ bolsa_trabajo/tests.py
@@ -2,7 +2,11 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.contrib import auth
 from models.enterprise import Enterprise
+from models.student import Student
+from models.student_level import StudentLevel
 
+
+'''
 class NewEnterpriseTestCase(TestCase):
 
     fixtures = ['new_enterprise_testdata.json','test_data_enterprise.json']
@@ -41,19 +45,34 @@ class NewEnterpriseTestCase(TestCase):
     def test_data_enterprise_fixture(self):
         ent = Enterprise.objects.get(name='Enterprise1')
         self.assertEqual(ent.rut,'17.847.192-2')
+'''
+
+class NewStudentTestCase(TestCase):
+
+    fixtures = ['test_data_student_level.json']
+
+    def test_new_student_view(self):
+        resp = self.client.get('/account/register/student/')
+        self.assertEqual(200,resp.status_code)
+
+    def test_new_student_register(self):
+        # create dictionary with new enterprise info
+        new_student_data = {'first_name':'Test', 'last_name':'Student', 'email':'dleytons@gmail.com', 'level':1, 'resume':'resumen alumno test 1', 'username':'test-student', 'password':'test-student', 'repeat_password':'test-student'}
         
-class OffersViewsTestCase(TestCase):
-    fixtures = ['offer_views_testdata.json']
+        # do a POST request including the new enterprise to be registered
+        resp = self.client.post('/account/register/student/',new_student_data)
+        
+        # get the new Student object from the database
+        new_student = Student.objects.get(username='test-student')
+        
+        # assert that the Enterprise object has the expected username
+        self.assertEqual(new_student.first_name,'Test')
+        
+        # when logging in using the new enterprise username and password, the login function should return True
+        self.assertTrue(self.client.login(username='test-student',password='test-student'))
 
-    def test_offer(self):
-        enterprise = Enterprise(name="foobar")
-        enterprise.save()
-        enterprises = Enterprise.objects.all()
-        print(type(enterprises))
-        for e in enterprises:
-            print("record...")
-            print(e.name)
-
-        Enterprise.objects.get(name='pepito')
-        resp = self.client.get('/offer/')
-        self.assertEqual(resp.status_code, 200)
+'''
+    def test_data_enterprise_fixture(self):
+        ent = Enterprise.objects.get(name='Enterprise1')
+        self.assertEqual(ent.rut,'17.847.192-2')
+'''
