4363716239a9c34911f237647ce0f801f3c2a1eb
gonzalo munoz <zamuz27@gmail.com>
2011-05-29 18:19:45 -0400

    modified fixture offers.json, and test_search_positive in tests/offer.py (should be working now)

--- bolsa_trabajo/tests/offer.py
+++ bolsa_trabajo/tests/offer.py
@@ -9,13 +9,13 @@ class OfferTestCase(TestCase):
 
     fixtures = ['users.json', 'enterprises.json', 'tags.json', 'offers.json']
 
-    def test_search(self):
+    def test_search_positive(self):
         get_data = {
             'enterprise':3,
-            'liquid_salary': 300000,
+            'liquid_salary': 75000,
             'include_unavailable_salaries':'on',
-            'tags':'Tag2'}
+            'tags':'Tag1'}
 
-        resp = self.client.get('/offer/', get_data)
-
+        resp = self.client.get('/offer/',get_data)
         self.assertEqual(200,resp.status_code)
+        self.assertFalse('No se encontraron ofertas' in resp.content)
