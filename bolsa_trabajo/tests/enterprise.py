38a4553f433ba644706504e29911983d9d67a216
Diego Leyton <dleytons@gmail.com>
2011-06-03 11:31:11 -0400

    se agrego el test para registrar la empresa desde la vista de un nuevo usuario

--- bolsa_trabajo/tests/enterprise.py
+++ bolsa_trabajo/tests/enterprise.py
@@ -8,7 +8,8 @@ from bolsa_trabajo.models.student_level import StudentLevel
 import settings
 import hashlib
 
-class NewEnterpriseTestCase(TestCase):
+#Test to register a new enterprise from the admin's view
+class AdminNewEnterpriseTestCase(TestCase):
 
     fixtures = ['users.json','enterprises.json']
 
@@ -32,7 +33,7 @@ class NewEnterpriseTestCase(TestCase):
 
         # assert that the Enterprise object has the expected username
         self.assertEqual(new_enterprise.username,'test-enterprise')
-        
+
         # assert that the Enterprise object is active
         self.assertEqual(new_enterprise.is_active,True)
 
@@ -41,10 +42,45 @@ class NewEnterpriseTestCase(TestCase):
 
         # when logging in using the new enterprise username and password, the login function should return True
         self.assertTrue(self.client.login(username='test-enterprise',password='test-enterprise'))
-        
+
     def test_data_enterprise_fixture(self):
         ent = Enterprise.objects.get(name='Enterprise1')
         self.assertEqual(ent.rut,'17.847.192-2')
+
+
+#Test to register a new enterprise from a new user's view
+class NewEnterpriseTestCase(TestCase):
+
+    fixtures = ['users.json','enterprises.json']
+
+    def test_new_enterprise_view(self):
+        resp = self.client.get('/account/register/enterprise/')
+        self.assertEqual(200,resp.status_code)
+
+    def test_new_enterprise_register(self):
+
+        # create dictionary with new enterprise info
+        new_enterprise_data = {'name':'Test Enterprise', 'rut':'12345678-9', 'phone':'1234567', 'address':'Fake Street 123', 'website':'http://www.example.com', 'description':'Test Enterprise description', 'first_name':'Test', 'last_name':'Enterprise', 'email':'test@example.com', 'username':'test-enterprise', 'password':'test-enterprise', 'repeat_password':'test-enterprise'}
+
+        # do a POST request including the new enterprise to be registered
+        resp = self.client.post('/account/register/enterprise/',new_enterprise_data)
+
+        # get the new enterprise object from the database
+        new_enterprise = Enterprise.objects.get(name='Test Enterprise')
+
+        # assert that the enterprise object has the expected username
+        self.assertEqual(new_enterprise.username,'test-enterprise')
+
+        # assert that the enterprise object is not active, the new user should not logging in, so the login function should return False
+        self.assertFalse(self.client.login(username='test-enterprise',password='test-enterprise'))
+
+        # activate the Enterprise object
+        new_enterprise.is_active = True
+        new_enterprise.save()
+
+        # when logging in using the new enterprise username and password, the login function should return True
+        self.assertTrue(self.client.login(username='test-enterprise',password='test-enterprise'))
+
 
 class PublishEnterpriseTestCase(TestCase):
 
@@ -58,37 +94,37 @@ class PublishEnterpriseTestCase(TestCase):
         u1.profile = p
         p.save()
         u1.save()
-        
+
         # login as test staff user
         self.client.login(username='test',password='test')
-        
+
         # go to pending enterprise requests
         resp = self.client.get('/account/pending_enterprise_request/')
-        
+
         # Enterprise3 should be displayed
         self.assertTrue('Enterprise3' in resp.content)
-        
+
     def test_accept_pending_request(self):
         # login as test staff user
         self.client.login(username='test',password='test')
-        
+
         # accept Enterprise3's request
         resp = self.client.get('/account/pending_enterprise_request/4/accept/')
-        
+
         self.client.logout()
-        
+
         # Enterprise3 should be active
         enterprise3 = Enterprise.objects.get(name='Enterprise3')
         self.assertTrue(enterprise3.is_active)
-        
+
     def test_reject_pending_request(self):
         # login as test staff user
         self.client.login(username='test',password='test')
-        
+
         # reject Enterprise3's request
         resp = self.client.get('/account/pending_enterprise_request/4/reject/')
-        
+
         self.client.logout()
-        
+
         # Enterprise3 should be deleted from the database
         self.assertFalse(self.client.login(username='test-enterprise3',password='test'))
--- bolsa_trabajo/tests/student.py
+++ bolsa_trabajo/tests/student.py
@@ -8,39 +8,37 @@ from bolsa_trabajo.models.student_level import StudentLevel
 
 class NewStudentTestCase(TestCase):
 
-    fixtures = ['users.json','test_data_student_level.json','students.json']
+    fixtures = ['users.json','student_level.json','student.json']
 
     def test_new_student_view(self):
         resp = self.client.get('/account/register/student/')
         self.assertEqual(200,resp.status_code)
 
     def test_new_student_register(self):
-        
-        # create dictionary with new enterprise info
+
+        # create dictionary with new student info
         new_student_data = {'first_name':'Test', 'last_name':'Student', 'email':'dleytonddd@gmail.com', 'level':1, 'resume':'resumen alumno test 1', 'username':'test-student', 'password':'test-student', 'repeat_password':'test-student'}
 
         # do a POST request including the new enterprise to be registered
         resp = self.client.post('/account/register/student/',new_student_data)
 
-        # get the new Student object from the database
+        # get the new student object from the database
         new_student = Student.objects.get(username='test-student')
 
-        # assert that the Student object has the expected username
+        # assert that the student object has the expected username
         self.assertEqual(new_student.first_name,'Test')
-        
-        # assert that the Enterprise object is active
-        self.assertEqual(new_student.is_active,True)
-'''
-        # logout
-        self.client.logout()
 
-        # when logging in using the new enterprise username and password, the login function should return True
+		# assert that the student object is not active, the new user should not logging in, so the login function should return False
+        self.assertFalse(self.client.login(username='test-student',password='test-student'))
+
+        #activate the new student object
+        new_student.is_active = True
+        new_student.save()
+
+        # when logging in using the new student username and password, the login function should return True
         self.assertTrue(self.client.login(username='test-student',password='test-student'))
-'''
 
-'''
-    def test_data_enterprise_fixture(self):
-        ent = Enterprise.objects.get(name='Enterprise1')
-        self.assertEqual(ent.rut,'17.847.192-2')
-'''
 
+    def test_data_student_fixture(self):
+        ent = Student.objects.get(first_name='testName1')
+        self.assertEqual(ent.resume,'Lorem ipsumnderit')
