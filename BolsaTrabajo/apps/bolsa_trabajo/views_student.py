f1f2f8b427cbf47e4bf79eec7618d918eb9e393a
lemuus <lemuus@linux-ydj6.site>
2011-09-12 13:19:49 -0300

    fix indentation error

--- BolsaTrabajo/apps/bolsa_trabajo/views_student.py
+++ BolsaTrabajo/apps/bolsa_trabajo/views_student.py
@@ -11,16 +11,16 @@ from .views_account import student_login_required
 
 @student_login_required
 def apply_to_offer(request,offer_id):
-	offer = Offer.objects.get(pk=offer_id)
-	student = Student.objects.get(pk=request.user.id)
-	is_already_postulating = student.is_postulating_to(offer)
-	error_message = None
-	if not student.profile.approved:
-		error_message = u'Su perfil aun no ha sido aprobado'
-	elif is_already_postulating:
-		error_message = u'Usted ya esta postulando a esta oferta'
-	elif not student.has_cv:
-		error_message = u'Usted debe subir su CV antes de poder postular a una oferta'
+    offer = Offer.objects.get(pk=offer_id)
+    student = Student.objects.get(pk=request.user.id)
+    is_already_postulating = student.is_postulating_to(offer)
+    error_message = None
+    if not student.profile.approved:
+        error_message = u'Su perfil aun no ha sido aprobado'
+    elif is_already_postulating:
+        error_message = u'Usted ya esta postulando a esta oferta'
+    elif not student.has_cv:
+        error_message = u'Usted debe subir su CV antes de poder postular a una oferta'
     
     if request.method == 'POST':
         if error_message:
